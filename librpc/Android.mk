12a9621853a5c2b4bc0123ef5c206cacda34d575
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-03 04:12:51 -0500

    Cleanup audio and rpc designation assignments

--- librpc/Android.mk
+++ librpc/Android.mk
@@ -1,7 +1,7 @@
-QCOM_RPC_ROOT := $(call my-dir)
+RPC_HW_ROOT := $(call my-dir)
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
-    include $(QCOM_RPC_ROOT)/8x60/Android.mk
+    include $(RPC_HW_ROOT)/8x60/Android.mk
 else
-    include $(QCOM_RPC_ROOT)/qcom/Android.mk
+    include $(RPC_HW_ROOT)/qcom/Android.mk
 endif
