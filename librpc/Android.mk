4a3b65f57ba0dfa9be6418660cd92663e0f66a04
TwistedUmbrella <twistedumbrella@gmail.com>
2012-04-08 19:41:17 -0400

    Realign rpc with upstream to provide GPS capability

--- librpc/Android.mk
+++ librpc/Android.mk
@@ -1,7 +1,34 @@
-RPC_HW_ROOT := $(call my-dir)
+LOCAL_PATH:= $(call my-dir)
 
-ifeq ($(BOARD_USES_QCOM_LIBRPC),true)
-    include $(RPC_HW_ROOT)/qcom/Android.mk
-else
-    include $(RPC_HW_ROOT)/msm7k/Android.mk
-endif
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= xdr.c rpc.c svc.c clnt.c ops.c svc_clnt_common.c
+
+LOCAL_C_INCLUDES:=$(LOCAL_PATH)
+
+LOCAL_CFLAGS:= -fno-short-enums 
+
+LOCAL_CFLAGS+=-DRPC_OFFSET=0
+#LOCAL_CFLAGS+=-DDEBUG -DVERBOSE
+
+LOCAL_COPY_HEADERS_TO:= librpc/rpc
+LOCAL_COPY_HEADERS:= \
+	rpc/clnt.h \
+	rpc/pmap_clnt.h \
+	rpc/rpc.h \
+	rpc/rpc_router_ioctl.h \
+	rpc/svc.h \
+	rpc/types.h \
+	rpc/xdr.h
+
+LOCAL_MODULE:= librpc
+
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := librpc
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_STATIC_LIBRARIES := libpower
+LOCAL_WHOLE_STATIC_LIBRARIES := librpc
+# LOCAL_PRELINK_MODULE := false
+include $(BUILD_SHARED_LIBRARY)
